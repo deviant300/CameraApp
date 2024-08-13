08157b25a7302d1a6a5c7618b92267e04e3fd8c2
deviant300 <alankritprateek@gmail.com>
2024-08-13 19:10:28 +0530

    Need to debug so removed code

---
+++ build/CMakeFiles/CameraApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CameraApp.dir/main.cpp.o"
+  "CMakeFiles/CameraApp.dir/main.cpp.o.d"
+  "CameraApp"
+  "CameraApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CameraApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -36,10 +36,12 @@ class CameraHandler : public IDeviceCallback {
     }
 
     void Initialize(){
-        CrError err = Init();
-        if(CR_FAILED(err)){
-            std::cout << "No cameras detected,exiting..." << std::endl;
-            std::cout << "Connect a camera and retry." << std::endl;
+
+        std::cout << "Attempting to initialize SDK" <<std::endl;
+        bool ret = Init();
+        if(!ret){
+            std::cout << "Initialization failed" << std::endl;
+            std::cout << "Retry." << std::endl;
             Release();
             std::exit(EXIT_FAILURE);
         }
@@ -53,68 +55,22 @@ class CameraHandler : public IDeviceCallback {
         return camera_handle;
     }
 
-    void Connect(){
-        std::cout << "Enumerating cameras...";
-        ICrEnumCameraObjectInfo* camera_list = nullptr;
-        auto enum_status = EnumCameraObjects(&camera_list);
-        if (CR_FAILED(enum_status) || camera_list == nullptr) {
-            std::cout << "No cameras detected,exiting..." << std::endl;
+    void enumerateCamera(){
+        std::cout << "Enumerating cameras..." << std::endl;
+        ICrEnumCameraObjectInfo* pEnum = nullptr;
+        CrError err = EnumCameraObjects(&pEnum);
+        if (pEnum == 0) {
+            std::cerr << "error: " << err << std::endl;
+            std::cout << "No cameras detected, exiting..." << std::endl;
             std::cout << "Connect a camera and retry." << std::endl;
             Release();
             std::exit(EXIT_FAILURE);
         }
 
-        auto camera_info = camera_list->GetCameraObjectInfo(0);
-        if (camera_info == nullptr){
-            std::cout <<"Camera detected but could not connect..." << std::endl;
-            std::cout << "Reconnect camera and retry." << std::endl;
-            Release();
-            std::exit(EXIT_FAILURE);
-        }
-
-        std::cout << "Camera detected: " << camera_info << std::endl;
-        const ICrCameraObjectInfo *pcamera = camera_info;
-        CameraHandler *cb = new CameraHandler();
-        CrDeviceHandle hDev = NULL;
-        CrError err = SCRSDK::Connect(const_cast<ICrCameraObjectInfo*>(pcamera), cb, &hDev);
-
-        CrDeviceHandle handle;
-        camera_handle = handle;
-        
-        DeviceConnectionVersion version;
-        DeviceConnectionVersion ver = version;
-
-        if(CR_FAILED(err)){
-            std::cout << "Camera not connected due to error:" << err << std::endl;
-            std::cout << "Try again later." << std::endl;
-            Release();
-            std::exit(EXIT_FAILURE);
-        }
-    }
-    
-    void disConnect(){
-        std::cout << "Disconnecting camera..." << std::endl;
-        Disconnect(camera_handle);
-        std::cout << "Releasing SDK..." << std::endl;
-        Release();
-        std::exit(0);
-    }
-    
-    void CaptureImage(){
-        if (!camera_connected){
-            std::cout << "No camera is connected, exiting..." << std::endl;
-        }
-
-        std::cout << "clicking shutter..." << std::endl;
-        CrError err = SendCommand(camera_handle, CrCommandId_Release, CrCommandParam_Down);
-        if (CR_FAILED(err)){
-            std::cout << "Shutter could not be clicked, error: " << err << std::endl;
-        }
-
-        std::cout << "releasing shutter..." << std::endl;
-        err = SendCommand(camera_handle, CrCommandId_Release, CrCommandParam_Up);
-        if (CR_FAILED(err)){
-            std::cout << "Shutter could not be released, error: " << err << std::endl;
+        CrInt32u count = pEnum->GetCount();
+        std::cout << count << " cameras detected" <<std::endl;
+        for(CrInt32u n = 0; n < count; n++){
+            const ICrCameraObjectInfo *pobj = pEnum->GetCameraObjectInfo(n);
         }
     }
 
@@ -131,49 +87,5 @@ int main(){
     CameraHandler handle;
     
     handle.Initialize();
-    handle.Connect();
-    handle.OnConnected();
-
-    if (handle.isCameraConnected()){
-        std::cout << "Proceeding" << std::endl;
-    }
-    else {
-        std::cerr << "No camera detected exiting..." << std::endl;
-        std::exit(0);
-        return 0;
-    }
-
-    CrChar* filepath = "C:/Users/alank/Documents/CameraApp/Images";
-    handle.setSaveInfo(*(const_cast<CrChar*>(filepath)));
-
-    while(true){
-        std::string input;
-
-        std::cout << "Take a picture (y/n):" << std::endl;
-        std::cin >> input;
-        std::cout << " " << std::endl;
-
-        if (handle.isCameraConnected() == true){
-            continue;
-        }
-        else {
-            break;
-        }
-
-        if (input == "y"){
-            handle.CaptureImage();
-
-            std::cout << "Image captured and saved to: " << filepath << std::endl;
-        }
-        else if (input == "n"){
-            std::cout << "Disconnecting..." << std::endl;
-            handle.disConnect();
-            break;
-        }
-        else {
-            std::cout << "Invalid input..." << std::endl;
-            continue;
-        }
-    }
-
+    handle.enumerateCamera();
 }
